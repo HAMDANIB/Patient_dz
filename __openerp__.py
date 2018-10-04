829090790cb53485e984bbe7c45f674958016c42
HAMDANI Belkacem <37189411+HAMDANIB@users.noreply.github.com>
2018-10-04 12:33:10 +0200

    main menu create

---
+++ __openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Gestion des patient',
+    'version': '1.1',
+    'category': 'Sante',
+    'sequence': 75,
+    'summary': 'patient_dz',
+    'description': """
+Gestion des patient
+    """,
+    'website': '',
+    'images': [
+
+    ],
+    'depends': [
+
+    ],
+    'data': [
+        'view.xml',
+    ],
+    'demo': [''],
+    'test': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [],
+}
